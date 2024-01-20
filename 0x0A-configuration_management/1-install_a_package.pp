f462b4fb51d79d905ace269f83df5557e677c9f5
OTHMANE <othmane1603@gmail.com>
2024-01-20 16:28:35 +0100

    all tasks

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,17 @@
+# Install an especific version of flask (2.1.0)
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
