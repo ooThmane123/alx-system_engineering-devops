4f6addff8208c9497edd9e6ac1950092e807dd8d
OTHMANE <othmane1603@gmail.com>
2024-02-21 23:07:32 +0100

    all tasks

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Returns TODo list information for a given employee ID """
+import requests
+import sys
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Returns TODo list information for a given employee ID """
+import requests
+import sys
+import csv
